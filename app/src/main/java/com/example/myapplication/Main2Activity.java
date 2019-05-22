8c6710f5ca9145bef994cc631cd97d9111baf13e
HyeYeon <gpdus335@naver.com>
2019-05-22 16:56:22 +0900

    마커 여러개 생성 중.. 잘 안 되는 중 ..

--- app/src/main/java/com/example/myapplication/Main2Activity.java
+++ app/src/main/java/com/example/myapplication/Main2Activity.java
@@ -9,11 +9,12 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 
 public class Main2Activity extends AppCompatActivity
-        implements OnMapReadyCallback {
+        implements OnMapReadyCallback, GoogleMap.OnMapClickListener {
     // 구글맵
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,6 +64,76 @@ public class Main2Activity extends AppCompatActivity
         map.moveCamera(CameraUpdateFactory.newLatLng(cafe4_3));
 
 
-        map.animateCamera(CameraUpdateFactory.zoomTo(10));
+        // 4번째
+        LatLng cafe4_4 = new LatLng(37.562376, 126.982463);
+
+        MarkerOptions markerOptions4_4 = new MarkerOptions();
+        markerOptions4_4.position(cafe4_4);
+        markerOptions4_4.title("더 스팟 패뷸러스");
+        //  markerOptions1.snippet("한국의 수도");
+        map.addMarker(markerOptions4_4);
+        map.moveCamera(CameraUpdateFactory.newLatLng(cafe4_4));
+
+        // 5번째
+        LatLng cafe4_5 = new LatLng(37.563195, 126.983741);
+
+        MarkerOptions markerOptions4_5 = new MarkerOptions();
+        markerOptions4_5.position(cafe4_5);
+        markerOptions4_5.title("포엠");
+        //  markerOptions1.snippet("한국의 수도");
+        map.addMarker(markerOptions4_5);
+        map.moveCamera(CameraUpdateFactory.newLatLng(cafe4_5));
+
+        // 6번째
+        LatLng cafe4_6 = new LatLng(37.564653, 126.989968);
+
+        MarkerOptions markerOptions4_6 = new MarkerOptions();
+        markerOptions4_6.position(cafe4_6);
+        markerOptions4_6.title("MWM");
+        //  markerOptions1.snippet("한국의 수도");
+        map.addMarker(markerOptions4_6);
+        map.moveCamera(CameraUpdateFactory.newLatLng(cafe4_6));
+
+        // 7번째
+        LatLng cafe4_7 = new LatLng(37.548935, 126.977531);
+
+        MarkerOptions markerOptions4_7 = new MarkerOptions();
+        markerOptions4_7.position(cafe4_7);
+        markerOptions4_7.title("카페 후암동");
+        //  markerOptions1.snippet("한국의 수도");
+        map.addMarker(markerOptions4_7);
+        map.moveCamera(CameraUpdateFactory.newLatLng(cafe4_7));
+
+        // 8번째
+        LatLng cafe4_8 = new LatLng(37.547809, 126.975636);
+
+        MarkerOptions markerOptions4_8 = new MarkerOptions();
+        markerOptions4_8.position(cafe4_8);
+        markerOptions4_8.title("MND coffee");
+        //  markerOptions1.snippet("한국의 수도");
+        map.addMarker(markerOptions4_8);
+        map.moveCamera(CameraUpdateFactory.newLatLng(cafe4_8));
+
+        // 9번째
+        LatLng cafe4_9 = new LatLng(37.544676, 126.973164);
+
+        MarkerOptions markerOptions4_9 = new MarkerOptions();
+        markerOptions4_9.position(cafe4_9);
+        markerOptions4_9.title("MUJABEE");
+        //  markerOptions1.snippet("한국의 수도");
+        map.addMarker(markerOptions4_9);
+        map.moveCamera(CameraUpdateFactory.newLatLng(cafe4_9));
+
+
+
+        map.animateCamera(CameraUpdateFactory.zoomTo(11));
+    }
+
+    public Marker addMarker(MarkerModel markerModel, boolean )
+
+    @Override
+    public void onMapClick(LatLng latLng) {
+
+
     }
 }
